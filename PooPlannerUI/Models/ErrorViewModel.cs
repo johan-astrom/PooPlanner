b37370a64c2028b49f7af5bbec9992b6ab0ccdc0
Johan Åström <johan.astrom@relight.se>
2022-04-28 09:31:43 +0200

    Added modules and MVC project

---
+++ PooPlannerUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PooPlannerUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
