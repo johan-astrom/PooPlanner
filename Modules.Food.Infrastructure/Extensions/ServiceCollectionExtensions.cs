3fcd6be28aff7ad9f85ea27c32a7addfbbe6c4e3
Johan Åström <johan.astrom@relight.se>
2022-04-28 14:11:44 +0200

    Finished extensions for Dishes module

---
+++ Modules.Food.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Modules.Dishes.Core.Abstractions;
+using Modules.Dishes.Infrastructure.Persistence;
+
+namespace Shared.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDishesInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddDatabaseContext<DishDbContext>(configuration)
+                .AddScoped<IDishDbContext>(provider => provider.GetService<DishDbContext>());
+            return services;
+        }
+    }
+}
---
+++ Modules.Food/Extensions/ModuleExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Infrastructure.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.Dishes.Extensions
+{
+    public static class ModuleExtensions
+    {
+        public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.
+                AddDishesInfrastructure(configuration);
+            return services;
+        }
+
+    }
+}
