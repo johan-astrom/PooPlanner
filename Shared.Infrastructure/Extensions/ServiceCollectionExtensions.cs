023e02a7cfd9a3083b968b2679371d23926c23b1
Johan Åström <johan.astrom@relight.se>
2022-04-28 13:35:39 +0200

    Added shared infrastructure extensions

--- Shared.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Shared.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,14 +1,37 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Infrastructure.Controller;
 
 namespace Shared.Infrastructure.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection
+        public static IServiceCollection AddSharedInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers()
+                .ConfigureApplicationPartManager(manager =>
+                {
+                    manager.FeatureProviders.Add(new InternalControllerFeatureProvider());
+                });
+            return services;
+        }
+
+        public static IServiceCollection AddDatabaseContext<T>(this IServiceCollection services, IConfiguration configuration)where T : DbContext
+        {
+            var connectionString = configuration.GetConnectionString("Default");
+            services.AddMSSQL<T>(connectionString);
+            return services;
+        }
+
+        public static IServiceCollection AddMSSQL<T>(this IServiceCollection services, string connectionString) where T : DbContext
+        {
+            services.AddDbContext<T>(m => m.UseSqlServer(connectionString, e => e.MigrationsAssembly(typeof(T).Assembly.FullName)));
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<T>();
+            dbContext.Database.Migrate();
+            return services;
+        }
     }
 }
