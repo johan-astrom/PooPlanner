247b29f590bab92847ba0d2febdff4f81f765eb3
Johan Åström <johan.astrom@relight.se>
2022-04-28 15:59:15 +0200

    Setting up generic repositories and dbcontexts

---
+++ Shared.Core/Repository/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Shared.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Core.Repository
+{
+    internal class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Find(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<long> ids)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
